b4a21ae08da0df598b785a08a8c46d9163094e61
MortezaFathnia <fathniamorteza@gmail.com>
2024-03-21 04:42:40 +0330

    skills page added

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,24 +1,25 @@
 import {
-    timestamp,
-    pgTable,
-    text,
-    primaryKey,
-   integer
-  } from "drizzle-orm/pg-core"
-  import type { AdapterAccount } from '@auth/core/adapters'
-  
-  export const users = pgTable("user", {
-   id: text("id").notNull().primaryKey(),
-   name: text("name"),
-   email: text("email").notNull(),
-   emailVerified: timestamp("emailVerified", { mode: "date" }),
-   image: text("image"),
-   firstName:text('first_name'),
-   lastName:text('last_name'),
-   jobTitle:text('job_title')
-  })
-  
-  export const accounts = pgTable(
+  timestamp,
+  pgTable,
+  text,
+  primaryKey,
+  integer
+} from "drizzle-orm/pg-core"
+import type { AdapterAccount } from '@auth/core/adapters'
+import { relations } from "drizzle-orm"
+
+export const users = pgTable("user", {
+  id: text("id").notNull().primaryKey(),
+  name: text("name"),
+  email: text("email").notNull(),
+  emailVerified: timestamp("emailVerified", { mode: "date" }),
+  image: text("image"),
+  firstName: text('first_name'),
+  lastName: text('last_name'),
+  jobTitle: text('job_title')
+})
+
+export const accounts = pgTable(
   "account",
   {
     userId: text("userId")
@@ -32,30 +33,66 @@ import {
     expires_at: integer("expires_at"),
     token_type: text("token_type"),
     scope: text("scope"),
-     id_token: text("id_token"),
+    id_token: text("id_token"),
     session_state: text("session_state"),
   },
   (account) => ({
     compoundKey: primaryKey({ columns: [account.provider, account.providerAccountId] }),
   })
-  )
-  
-  export const sessions = pgTable("session", {
-   sessionToken: text("sessionToken").notNull().primaryKey(),
-   userId: text("userId")
-     .notNull()
-     .references(() => users.id, { onDelete: "cascade" }),
-   expires: timestamp("expires", { mode: "date" }).notNull(),
+)
+
+export const sessions = pgTable("session", {
+  sessionToken: text("sessionToken").notNull().primaryKey(),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  expires: timestamp("expires", { mode: "date" }).notNull(),
+})
+
+export const verificationTokens = pgTable(
+  "verificationToken",
+  {
+    identifier: text("identifier").notNull(),
+    token: text("token").notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull(),
+  },
+  (vt) => ({
+    compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
   })
-  
-  export const verificationTokens = pgTable(
-   "verificationToken",
-   {
-     identifier: text("identifier").notNull(),
-     token: text("token").notNull(),
-     expires: timestamp("expires", { mode: "date" }).notNull(),
-   },
-   (vt) => ({
-     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-   })
-  )
+)
+
+export const skills = pgTable('skills', {
+  id: text('id').notNull().primaryKey(),
+  name: text('name').notNull().unique(),
+})
+
+export const usersToSkills = pgTable(
+  'users to skills',
+  {
+    userId: text('user_id').notNull(),
+    skillId: text('skill_id').notNull(),
+    rating: integer('rating')
+  },
+  (us) => ({
+    compondKey: primaryKey({ columns: [us.userId, us.skillId] })
+  })
+)
+
+export const userRelations = relations(users, ({ many }) => ({
+  usersToUsersSkills: many(usersToSkills)
+}))
+
+export const skillsRelations = relations(skills, ({ many }) => ({
+  skilsToUsersSkills: many(usersToSkills)
+}))
+
+export const usersToSkillsRelations=relations(usersToSkills,(({one})=>({
+  skill:one(skills,{
+    fields:[usersToSkills.skillId],
+    references:[skills.id]
+  }),
+  user:one(users,{
+    fields:[usersToSkills.userId],
+    references:[users.id]
+  })
+})))
